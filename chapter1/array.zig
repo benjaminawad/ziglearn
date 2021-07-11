a7457990e595b171df027f89622c2b4cab40ddd3
Benjamin Awad <awad@sipgate.de>
2021-07-11 15:50:11 +0200

    Format chapter1/array.zig using zig fmt

--- chapter1/array.zig
+++ chapter1/array.zig
@@ -1,8 +1,20 @@
 const std = @import("std");
 
 pub fn main() void {
-    const a = [5]u8{'H', 'e', 'l', 'l', 'o',};
-    const b = [_]u8{'w', 'o', 'r', 'l', 'd',};
+    const a = [5]u8{
+        'H',
+        'e',
+        'l',
+        'l',
+        'o',
+    };
+    const b = [_]u8{
+        'w',
+        'o',
+        'r',
+        'l',
+        'd',
+    };
     const length = array.len;
-    std.debug.print("{s}, {s}!\n", .{a, b});
+    std.debug.print("{s}, {s}!\n", .{ a, b });
 }
